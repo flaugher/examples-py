93957bfa14cd11ccc330940bb63b9efb9d7ad39f
Robert Flaugher <rlfprog@gmail.com>
2015-08-06 10:55:14 -0700

    Call is_prime from main

--- primes.py
+++ primes.py
@@ -1,9 +1,17 @@
+#!/usr/bin/env python
 
 def is_prime(num):
     '''Determine if a number is prime.'''
     for x in range(2, num):
         if num % x == 0:
-            return False;
-    return True;
+            # return False
+            print "%s is not a prime number" % num
+    # return True
+    print "%s is a prime number" % num
 
-print is_prime(4)
+def main():
+    '''Main'''
+    is_prime(5)
+
+if __name__ == "__main__":
+    main()
