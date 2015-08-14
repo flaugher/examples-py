904456a1e7b93104b1a941cca6244882e89d6e0d
Robert Flaugher <rlfprog@gmail.com>
2015-08-14 15:54:19 -0700

    Added second decorated function to decorators

--- decorators.py
+++ decorators.py
@@ -4,9 +4,9 @@
 def my_decorator(f):
     """Decorator that prints comments before and after function."""
     def wrapper(*args, **kwargs):
-        print "About to run function"
+        print "About to run function %s" % f.__name__
         result = f(*args, **kwargs)
-        print "Just ran function"
+        print "Just ran function %s" % f.__name__
         return result
     return wrapper
 
@@ -15,6 +15,11 @@ def add(x, y):
     """Add two numbers."""
     return x + y
 
+@my_decorator
+def subtract(x, y):
+    """Subtract one number from another."""
+    return x - y
+
 if __name__ == "__main__":
-    sum = add(1, 1)
-    print sum
+    print "Sum is %d" % add(1, 1)
+    print "Difference is %d" % subtract(2, 1)
