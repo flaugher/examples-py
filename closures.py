15db4d5ce1d4dcbbe22fecff2d0a32c4d424e709
Robert Flaugher <rlfprog@gmail.com>
2015-08-14 14:57:24 -0700

    Added power function to closures

--- closures.py
+++ closures.py
@@ -2,25 +2,37 @@
 # -*- coding: utf-8 -*-
 import os, sys
 
-"""Examples of closures.
+def make_power(n):
+    """Define a closure that raises a number to the power of n."""
+    def nth_power(x):
+        return x**n
+    return nth_power
 
-Closures can be used to:
-- Replace hard-coded constants
-- Replace global variables
+def call_make_power():
+    squared = make_power(2)
+    print squared(2)
+    cubed = make_power(3)
+    print cubed(2)
 
-"""
 def make_inc(x):
-    """Increment a number by another number."""
+    """Increment a number by another number.
+
+    The call to makeInc creates a binding for x that is
+    referenced inside the function inc. Each call to makeInc 
+    creates a new instance of this function, but each instance 
+    has a link to a different binding of x.
+
+    """
     def inc(y):
         # x is "closed" in the definition of inc.
         # When inc is defined, it knows what x is because
         # x was passed to its enclosing function make_inc.
-        # Whenever x is called, it will increment whatever
+        # Whenever x is called, inc will increment whatever
         # is passed to it via "y" by the value of x.
         return y + x
     return inc
 
-def main():
+def call_make_inc():
     # This creates a closure function called "inc5" that always
     # increments the number passed to it by 5
     inc5 = make_inc(5)
@@ -30,6 +42,10 @@ def main():
     # increments the number passed to it by 10
     inc10 = make_inc(10)
     print inc10(5)
+
+def main():
+    call_make_inc()
+    call_make_power()
 
 if __name__ == "__main__":
     main()
